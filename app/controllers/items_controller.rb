9a9d5e785e75ce5e3994a3349faaee361e169116
ksk-o <69468089+ksk-o@users.noreply.github.com>
2020-11-22 20:01:40 +0900

    商品出品機能の実装
    ・バリデーション設定
    ・ログイン有無により出品画面への遷移
    ・出品時のエラーメッセージ表示
    ・商品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
+  def new
+    @item = Item.new
+  end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(
+      :image, 
+      :name, 
+      :description, 
+      :category_id, 
+      :condition_id, 
+      :postage_id, 
+      :shipping_plase_id, 
+      :ship_date_id, 
+      :price
+    ).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,15 +10,16 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_plase
   belongs_to_active_hash :ship_date
   
-  # with_options presence: true do
-  #   validates :name,               
-  #   validates :description,        
-  #   validates :category_id,        numericality: { other_than: 1 }, 
-  #   validates :condition_id,       numericality: { other_than: 1 }, 
-  #   validates :postage_id,         numericality: { other_than: 1 }, 
-  #   validates :shipping_plase_id,  numericality: { other_than: 1 }, 
-  #   validates :ship_date_id,       numericality: { other_than: 1 }, 
-  #   validates :price,              
-  # end
-
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id,        numericality: { other_than: 1 }
+    validates :condition_id,       numericality: { other_than: 1 }
+    validates :postage_id,         numericality: { other_than: 1 } 
+    validates :shipping_plase_id,  numericality: { other_than: 1 } 
+    validates :ship_date_id,       numericality: { other_than: 1 } 
+    validates :price,              format: {with: /\A[0-9]+\z/ }, numericality: 
+                                    {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
 end
