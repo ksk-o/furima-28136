acb06d1e7bdfc43a746fb880da7668354e7cb013
ksk-o <69468089+ksk-o@users.noreply.github.com>
2020-12-13 22:56:38 +0900

    商品購入機能_修正_20201213-2
    user_idとitem_idをバリデーションへ追記し、テストコードの修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -8,6 +8,8 @@ class OrderForm
     validates :city
     validates :block
     validates :phone_number,  format: { with: /\A\d{11}\z/ }
+    validates :user_id
+    validates :item_id
     validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1 }
--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe OrderForm, type: :model do
   describe '商品購入機能' do
     
     before do
-      @order_form = FactoryBot.build(:order_form)
+      @user = FactoryBot.build_stubbed(:user)
+      @item = FactoryBot.build_stubbed(:item)
+      @order_form = FactoryBot.build(:order_form, user_id: @user.id, item_id: @item.id)
+      
     end
 
     it "すべての値が正しく入力されていれば保存できること" do
@@ -64,8 +67,17 @@ RSpec.describe OrderForm, type: :model do
       expect(@order_form.errors.full_messages).to include("Token can't be blank")
     end
 
+    it "user_idが空では登録できないこと" do
+      @order_form.user_id = nil
+      @order_form.valid?
+      expect(@order_form.errors.full_messages).to include("User can't be blank")
+    end
+
+    it "item_idが空では登録できないこと" do
+      @order_form.item_id = nil
+      @order_form.valid?
+      expect(@order_form.errors.full_messages).to include("Item can't be blank")
+    end
+
   end
 end
-
-
- 
